fc3ab5962dbb1804052627ecfa3867b14fd844b9
Francílio Araújo <francilioaraujo@gmail.com>
2020-02-10 21:58:48 +0000

    Comandos para criar usuários

---
+++ tyro/create-context-development.sh
@@ -0,0 +1,1 @@
+kubectl config set-context developer --kubeconfig=/root/.kube/config --user=drogo --cluster=kubernetes
---
+++ tyro/create-user-drogo.sh
@@ -0,0 +1,1 @@
+kubectl config set-credentials drogo --kubeconfig=/root/.kube/config --client-key=/root/drogo.key --client-certificate=/root/drogo.crt
